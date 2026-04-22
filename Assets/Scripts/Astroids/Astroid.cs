bfefa3998fd88df6f9ec3cf2e6a46ab5064ee387
BazzyD <ofir3347@gmail.com>
2026-04-23 01:04:03 +0300

    Astroids
    create astroids in 5 levels
    astroids can explode to smaller astroids
    added astroids to pool

---
+++ Assets/Scripts/Astroids/Astroid.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BaseMovment))]
+public class Astroid : MonoBehaviour,IDamageable, IPoolable
+{
+    private BaseMovment _movment;
+
+    [Header("Astroid Stats")]
+    [SerializeField] public int astroidLevel =5;
+    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private float damageOnCollision = 25f;
+    private float _currentHealth;
+    [Header("Movement Stats")]
+    [SerializeField] private float maxSpeed = 5f;
+    [SerializeField] private float minSpeed = 2f;
+    [SerializeField] private float maxRotationSpeed = 200f;
+    [SerializeField] private float minRotationSpeed = 100f;
+    private int maxSpawnedAstroids = 5;
+    private int minSpawnedAstroids = 2;
+    private void Awake()
+    {
+        _movment = GetComponent<BaseMovment>();
+    }
+
+    public void OnSpawn()
+    {
+        _currentHealth = maxHealth;
+        float speed = Random.Range(minSpeed, maxSpeed);
+        float rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+        
+        _movment.ApplyInitialImpulse(transform.up * speed);
+        _movment.ApplyTorqueImpulse(rotationSpeed);
+    }
+    public void OnDespawn()
+    {
+
+        _movment.StopEverything();
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        _currentHealth -= damageAmount;
+        if (_currentHealth <= 0)
+        {
+            if(astroidLevel > 1) SpawnChildren();
+            ObjectPool.Instance.Despawn($"Astroid_lvl{astroidLevel}", gameObject);
+        }
+    }
+    private void SpawnChildren(){
+        int childrenAmount = Random.Range(minSpawnedAstroids, maxSpawnedAstroids+1);
+
+        for (int i = 0; i < childrenAmount; i++)
+        {
+            float randomZ = Random.Range(0f, 360f);
+            Quaternion randomRotation = Quaternion.Euler(0, 0, randomZ);
+            ObjectPool.Instance.Spawn($"Astroid_lvl{astroidLevel - 1}", transform.position, randomRotation);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // need to check player collision and apply damage to player only not for astroids
+        // if (collision.gameObject.TryGetComponent(out IDamageable player))
+        // {
+        //     TakeDamage(maxHealth); // Destroy the astroid on collision with player
+        //     player.TakeDamage(damageOnCollision);
+        // }
+    }
+}
--- Assets/Scripts/BaseComponents/BaseMovment.cs
+++ Assets/Scripts/BaseComponents/BaseMovment.cs
@@ -9,6 +9,7 @@ public class BaseMovment : MonoBehaviour
     //[SerializeField] private float maxRotationSpeed = 100f;
     private float _thrustForce;
     private float _rotationTorque;
+    private bool selfRotate = false;
     private Rigidbody2D rb;
     private void Awake()
     {
@@ -22,6 +23,7 @@ public class BaseMovment : MonoBehaviour
     }
     private void HandleThrust()
     {
+        if(selfRotate) return;
         rb.AddForce(_thrustForce * thrustSpeed * transform.up);
         if (rb.linearVelocity.magnitude > maxSpeed)
         {
@@ -30,6 +32,7 @@ public class BaseMovment : MonoBehaviour
     }
     private void HandleRotation()
     {
+        if(selfRotate) return;
         rb.angularVelocity = _rotationTorque * -rotationSpeed;
         // rb.AddTorque(_rotationInput * -rotationSpeed);
         // if (rb.angularVelocity > maxRotationSpeed)
@@ -41,6 +44,18 @@ public class BaseMovment : MonoBehaviour
         //     rb.angularVelocity = -maxRotationSpeed;
         // }
     }
+    public void ApplyInitialImpulse(Vector2 force)
+    {
+        rb.AddForce(force, ForceMode2D.Impulse);
+    }
+
+    public void ApplyTorqueImpulse(float torque)
+    {
+        // Randomize clockwise or counter-clockwise
+        selfRotate = true;
+        float direction = Random.value > 0.5f ? 1f : -1f;
+        rb.AddTorque(torque * direction, ForceMode2D.Impulse);
+    }
     public void SetForce(float force)
     {
         _thrustForce = force;
@@ -48,5 +63,10 @@ public class BaseMovment : MonoBehaviour
     public void SetTorque(float torque)
     {
         _rotationTorque = torque;
+    }
+    public void StopEverything()
+    {
+        rb.linearVelocity = Vector2.zero;
+        rb.angularVelocity = 0f;
     }
 }
